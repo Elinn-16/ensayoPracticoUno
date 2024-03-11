2bde8445c3d3b9f2f56de3c4a13878fb967c0035
Elinn-16 <martinaenievas@escuelasproa.edu.ar>
2024-03-10 22:14:31 -0300

    LISTOOO

--- scrip.js
+++ scrip.js
@@ -1,4 +1,5 @@
 const fotos = document.querySelectorAll('.foto');
+const imagenesSeleccionadas = [];
 
 fotos.forEach((foto) => {
     foto.addEventListener('click', () => {
@@ -10,5 +11,20 @@ fotos.forEach((foto) => {
         });
 
         foto.classList.add('clicked'); //Agregar la clase 'clicked' a la foto que es seleccionada
+    
+        if (imagenesSeleccionadas.length === 2) {
+            const primeraImagen = imagenesSeleccionadas[0];
+            const segundaImagen = imagenesSeleccionadas[1];
+
+            if (primeraImagen.id === segundaImagen.id) {
+                alert('¡Felicitaciones acertaste! ');
+                primeraImagen.remove();
+                segundaImagen.remove();
+                imagenesSeleccionadas = [];
+            } else {
+                imagenesSeleccionadas = [];
+            }
+        }
+
     });
 });
